38c861ea8ca1396c2e93e5db23216db652917364
Nikhil Singh Dhakad <nikhilsinghdhakad10@gmail.com>
2023-10-15 14:08:24 +0530

    Initial commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+export class NewsItem extends Component {
+  render() {
+   let  {title, description, imageUrl, newsUrl} = this.props;
+    return (
+      <div className="my-3">
+          <div className="card" >
+            <img src={!imageUrl?"https://static.vecteezy.com/system/resources/previews/000/681/858/original/live-breaking-news-report-banner.jpg":imageUrl} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title"> {title}</h5>
+              <p className="card-text">{description}</p>
+              <a rel = "noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+            </div>
+          </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
